abb0d9156ec0d1a83e299911dfc8024293d2544b
Henry Jonathan <henryjonathanchandra12@gmail.com>
2025-01-15 13:57:08 +0700

    add migrations and models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->enum('role', ['user', 'petugas', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
